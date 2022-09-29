65e60c01b45652e2263b9ff00f6540298de3ca54
jinhengyang <jinhengyang@camera360.com>
2022-09-29 16:04:59 +0800

    init project

---
+++ SubmoduleTest1/SubmoduleTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubmoduleTest1
+//
+//  Created by king on 2022/9/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
